2d89f0158ad09dfa2ddf564245033552221b373d
lecho1996 <abelaj18@gmail.com>
2021-04-25 12:02:18 -0400

    replaced README, added smart contract

--- FrontEnd/blackjack/src/App.js
+++ FrontEnd/blackjack/src/App.js
@@ -8,17 +8,6 @@ import GameContainer from '../src/Components/GameContainer/GameContainer'
 import styles from '../src/App.module.scss'
 
 function App() {
-       const [message, setMessage]= useState('Default Alert')
-
-       const [userCards, setUserCards]= useState([])
-       const [userScore, setUserScore]= useState(0)
-
-       const [dealerCards, setDealerCards]= useState([])
-       const [dealerScore, setDealerScore]= useState(0)
-
-       const [balance, setBalance]= useState(100)
-       const [bet, setBet]= useState(0);
-
 
   return (
          <div className={styles.App}>
