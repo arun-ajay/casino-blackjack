b6348261d9094b1fa17633a7dbcb7fd864e85877
arun-ajay <arunajay42795@gmail.com>
2021-04-28 22:39:16 -0400

    edited bot 2 and server to accommodate for 2 rcom and rp now

--- Bots/test.py
+++ Bots/test.py
@@ -1,10 +1,23 @@
-import sqlite3
+from Crypto.Random import random
+from web3 import Web3
 
 
-connection = sqlite3.connect("blackjack.db")
-cursor = connection.cursor()
-cursor.execute("SELECT * FROM GAMEDATA WHERE [ADR]  = ?",('0x2372F830e274e0f4B5CD7710d519B60eA06007CB',))
 
-data = cursor.fetchone()
+def rComBytes32():
+       rComNum = random.getrandbits(512) #int
+       rCom = bin(rComNum).split('b')[1] #string 
+       rcomhex = Web3.soliditySha3(["string"],[rCom]).hex()
+       return rcomhex
 
-print(data[1])
+
+def rpBytes32():
+       rp = random.getrandbits(312)
+       rp = bin(rp).split('b')[1]
+       rphex = Web3.soliditySha3(["string"],[rp]).hex()
+       return rphex
+
+
+
+print("RCOM BYTES 32:",rComBytes32())
+
+print("RP BYTES 32:",rpBytes32())
--- web3py/hash.py
+++
@@ -1,9 +0,0 @@
-from web3 import Web3
-
-hashInt = 10
-
-kek = Web3.soliditySha3(["uint256", "uint256"], [123, 321])
-
-print(kek)
-
-print(kek.hex())
